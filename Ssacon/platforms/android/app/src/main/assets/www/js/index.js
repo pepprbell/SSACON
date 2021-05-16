7d3b5bdc79af68f3aa0203584b072ecf1c2d5ee4
신민호(본인의 이름) <eeqwer123@gmail.com()>
2021-05-17 00:24:22 +0900

    [feat/FE] : git ignore 수정

--- Ssacon/platforms/android/app/src/main/assets/www/js/index.js
+++ Ssacon/platforms/android/app/src/main/assets/www/js/index.js
@@ -1,31 +1,41 @@
 var app = {
-  initialize: function() {
-      this.bindEvents();
+  initialize: function () {
+    this.bindEvents();
   },
 
-  bindEvents: function() {
-      document.addEventListener('deviceready', this.onDeviceReady.bind(this), false)
-      document.getElementById("worker").addEventListener("click", this.goWorkerPage.bind(this))
-      document.getElementById("manager").addEventListener("click", this.goManagerPage.bind(this))
-      document.getElementById("loginpage").addEventListener("click", this.goLoginPage.bind(this))
+  bindEvents: function () {
+    document.addEventListener(
+      "deviceready",
+      this.onDeviceReady.bind(this),
+      false
+    );
+    document
+      .getElementById("worker")
+      .addEventListener("click", this.goWorkerPage.bind(this));
+    document
+      .getElementById("manager")
+      .addEventListener("click", this.goManagerPage.bind(this));
+    document
+      .getElementById("loginpage")
+      .addEventListener("click", this.goLoginPage.bind(this));
   },
 
-  onDeviceReady: function() {
-      console.log('firstPage');
+  onDeviceReady: function () {
+    console.log("firstPage");
   },
 
-  goWorkerPage: function() {
-      window.location = "./template/testrouter/testrouter.html";
+  goWorkerPage: function () {
+    window.location = "./template/testrouter/testrouter.html";
   },
 
-  goManagerPage: function() {
-      window.location = "./template/monitor/monitor.html";
+  goManagerPage: function () {
+    window.location = "./template/monitor/monitor.html";
   },
 
-  goLoginPage: function() {
-      window.location = "./template/login/login.html";
+  goLoginPage: function () {
+    window.location = "./template/login/login.html";
   },
-}
+};
 
-app.initialize()
-window.app = app
+app.initialize();
+window.app = ap;
