8e447eede3dd5b775ef427060c80b11b6b615911
JeongHyunwoo <hw2621@daum.net>
2021-05-17 23:48:20 +0900

    [Feat] 모니터링 중간 완성

--- Ssacon/www/template/monitor/monitor.js
+++ Ssacon/www/template/monitor/monitor.js
@@ -1,8 +1,32 @@
 const beacon__map = document.querySelector(".Beacon__map");
+beacon__map.addEventListener("click", (event) => {
+    // 기존 네모 지우기
+    // console.log('네모 지우기')
+    const before_area = beacon__map.querySelector(".picked_area")
+    if(before_area) {
+        // console.log(before_area, '지움')
+        before_area.remove()
+    }
+    if(event.target.classList.contains("Beacon__item")) {
+        // console.log('네모만들어라')
+        // 새 네모 만들기
+        const now_area = document.createElement("div")
+        now_area.className = "picked_area"
+        now_area.style.width = "20%"
+        now_area.style.height = "20%"
+        // console.log(event.target.dataset.left)
+        // console.log(event.target.dataset.top)
+        now_area.style.left = `${event.target.dataset.left*1-8.5}%`
+        now_area.style.top = `${event.target.dataset.top*1-8.5}%`
+        // console.log(now_area.style.left,now_area.style.top)
+        // console.log(now_area)
+        beacon__map.appendChild(now_area)
+    }
+})
 const beacon__map__width = beacon__map.clientWidth;
 const beacon__map__height = beacon__map.clientHeight;
-const x_diff = [-4.5, -0.5, 3.5, -4.5, 3.5, -4.5, -0.5, 3.5];
-const y_diff = [-5, -5, -5, -1, -1, 3, 3, 3];
+const x_diff = [-7.5, -0.5, 6.5, -7.5, 6.5, -7.5, -0.5, 6.5];
+const y_diff = [-8, -8, -8, -1, -1, 6, 6, 6];
 let beacon__cnt = 9;
 
 var requestOptions = {
@@ -10,51 +34,8 @@ var requestOptions = {
   redirect: "follow",
 };
 let beacons = [];
-beacons = [
-  { xPos: 20, yPos: 20, connetWorkers: [1, 2, 3, 4, 5, 6, 7, 8] },
-  {
-    xPos: 20,
-    yPos: 50,
-    connetWorkers: [{ username: "신민호" }, { username: "정현우" }],
-  },
-  {
-    xPos: 20,
-    yPos: 80,
-    connetWorkers: [{ username: "박승범" }, { username: "구진범" }],
-  },
-  {
-    xPos: 50,
-    yPos: 20,
-    connetWorkers: [{ username: "신민호" }, { username: "정현우" }],
-  },
-  {
-    xPos: 50,
-    yPos: 50,
-    connetWorkers: [{ username: "박승범" }, { username: "구진범" }],
-  },
-  {
-    xPos: 50,
-    yPos: 80,
-    connetWorkers: [{ username: "신민호" }, { username: "정현우" }],
-  },
-  {
-    xPos: 80,
-    yPos: 20,
-    connetWorkers: [{ username: "박승범" }, { username: "구진범" }],
-  },
-  {
-    xPos: 80,
-    yPos: 50,
-    connetWorkers: [{ username: "신민호" }, { username: "정현우" }],
-  },
-  {
-    xPos: 80,
-    yPos: 80,
-    connetWorkers: [{ username: "박승범" }, { username: "구진범" }],
-  },
-];
 
-setInterval(() => {
+function Monitor() {
   fetch("http://k4b101.p.ssafy.io/api/monitoring/beacon", requestOptions)
     .then((response) => response.json())
     .then((result) => {
@@ -72,38 +53,89 @@ setInterval(() => {
       beacons.forEach((beacon) => {
         const beacon__item = document.createElement("div");
         beacon__item.className = "Beacon__item";
-        beacon__item.style.left = `${beacon.xPos}%`;
-        beacon__item.style.top = `${beacon.yPos}%`;
+        beacon__item.dataset.id = beacon.beaconId
+        beacon__item.dataset.left = beacon.xpos
+        beacon__item.dataset.top = beacon.ypos
+        beacon__item.style.left = `${beacon.xpos}%`;
+        beacon__item.style.top = `${beacon.ypos}%`;
         const beacon__img = document.createElement("img");
         const beacon__items = document.querySelectorAll(".Beacon__item");
-        // console.log(beacon__items)
-        if (beacon__items.length < 9) {
-          beacon__map.appendChild(beacon__item);
-          beacon__cnt += 1;
+        //아픈 비콘은 표시를 해줌
+        for(let i = 0; i < beacon__items.length; i++) {
+          if(beacon__items[i].dataset.id == beacon.beaconId) {
+              // 배터리 or 온습도
+              if(beacon.beaconBattery <= 5 || beacon.beaconMoisture > beacon.humiMax || beacon.beaconMoisture < beacon.humiMin 
+              || beacon.beaconTemperature > beacon.tempMax || beacon.beaconTemperature < beacon.tempMin) {
+                if(!beacon__items[i].classList.contains("bad")){
+                  beacon__items[i].classList.add("bad")
+                }
+              } else {
+                if(beacon__items[i].classList.contains("bad")){
+                  beacon__items[i].classList.remove("bad")
+                }
+              }
+              break
+          }
+      }
+        //이미 추가댄 비콘이면 안 넣음
+        let is_added = false
+        for(let i = 0; i < beacon__items.length; i++) {
+            if(beacon__items[i].dataset.id == beacon.beaconId) {
+                is_added = true
+                break
+            }
         }
+        if(!is_added) {
+            beacon__map.appendChild(beacon__item)
+        }
+        // console.log(beacon__items)
+        
 
         let cnt = 0;
-        // console.log(beacon);
-        if (beacon.connectWorkers.length > 0) {
-          beacon.connetWorkers.forEach((worker) => {
+        
+        if (beacon.connectWorkers !== undefined) {
+          beacon.connectWorkers.forEach((worker) => {
             const beacon__worker = document.createElement("div");
             beacon__worker.className = "Beacon__worker";
+            
             const beacon__worker__img = document.createElement("img");
             beacon__worker__img.className = "Beacon__worker__img";
             // beacon__worker.src="file:///android_asset/www/template/monitor/icons/person.png"
             beacon__worker__img.src = "./icons/person.png";
-
+            // 해당 비콘 온습도 문제 있으면 빨간색으로 바꾸기
             beacon__worker.className = "Beacon__worker";
             beacon__worker.appendChild(beacon__worker__img);
-            beacon__worker.addEventListener("click", () => {
-              // 여기다가 오른쪽 표에 내용 바꿔주세요
-            });
-            const x_pos = beacon.xPos + x_diff[cnt];
-            const y_pos = beacon.yPos + y_diff[cnt];
+          
+            const x_pos = beacon.xpos + x_diff[cnt];
+            const y_pos = beacon.ypos + y_diff[cnt];
             cnt += 1;
             // console.log(x_pos,y_pos,x_gob,y_gob)
             beacon__worker.style.left = `${x_pos}%`;
             beacon__worker.style.top = `${y_pos}%`;
+            beacon__map.appendChild(beacon__worker);
+          });
+        }
+        if (beacon.nonConnectWorkers !== undefined) {
+          beacon.nonConnectWorkers.forEach((worker) => {
+            const beacon__worker = document.createElement("div");
+            beacon__worker.className = "Beacon__worker";
+            
+            const beacon__worker__img = document.createElement("img");
+            beacon__worker__img.className = "Beacon__worker__img";
+            // beacon__worker.src="file:///android_asset/www/template/monitor/icons/person.png"
+            beacon__worker__img.src = "./icons/wrong.png";
+            // 해당 비콘 온습도 문제 있으면 빨간색으로 바꾸기
+            beacon__worker.className = "Beacon__worker";
+            beacon__worker.appendChild(beacon__worker__img);
+          
+            const x_pos = beacon.xpos + x_diff[cnt];
+            const y_pos = beacon.ypos + y_diff[cnt];
+            cnt += 1;
+            // console.log(x_pos,y_pos,x_gob,y_gob)
+            beacon__worker.style.left = `${x_pos}%`;
+            beacon__worker.style.top = `${y_pos}%`;
+            // console.log(x_pos,y_pos)
+            // console.log(beacon__worker.style.left, beacon__worker.style.top)
             beacon__map.appendChild(beacon__worker);
           });
         }
@@ -112,15 +144,15 @@ setInterval(() => {
     .then(() => {
       // fetch(알람다가져온후) 거기에 기존에잇느거 지우고 다시 넣기
       let alarmUserInfo = window.localStorage.getItem("userInfo");
-      console.log("확인", alarmUserInfo);
-      console.log(JSON.parse(alarmUserInfo));
+    //   console.log("확인", alarmUserInfo);
+    //   console.log(JSON.parse(alarmUserInfo));
       alarmUserInfo = JSON.parse(alarmUserInfo);
       let userId = alarmUserInfo.data.userId;
       fetch(`http://k4b101.p.ssafy.io/api/alarm/${userId}`, requestOptions)
         .then((response) => response.json())
         .then((result) => {
           alarmlist = result.data;
-          console.log("alarm:", alarmlist);
+        //   console.log("alarm:", alarmlist);
           alarmlist.forEach((alarm) => {
             let item = document.createElement("div");
             let left = document.createElement("div");
@@ -309,4 +341,9 @@ setInterval(() => {
           }
         });
     });
+}
+
+Monitor();
+setInterval(() => {
+  Monitor();
 }, 3000);
